4e76f0a680ed547238d3ca47370dc2b1aad32a89
LozinsekMatic1 <matic.lozinsek@student.um.si>
2021-01-04 14:30:38 +0100

    Update notepad Hello.cs

--- notepad Hello.cs
+++ notepad Hello.cs
@@ -5,5 +5,6 @@ public class Hello
     {
         System.Console.WiteLine("Hello, World!");
         System.Console.WiteLine("Hello, GitHub!");
+        System.Console.WiteLine("Hello, Local!");
     }
 }
